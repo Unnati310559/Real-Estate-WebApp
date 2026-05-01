48f6a38c4b5bfb2b614f1f5ffae5458f1a8f8cf6
Unnati310559 <unnatig860@gmail.com>
2026-05-01 10:49:21 +0530

    Enhance model loading with download and error handling
    Updated model loading to check for existence and download if missing, added error handling for loading.

--- services/prediction.py
+++ services/prediction.py
@@ -1,5 +1,6 @@
 import joblib
 import os
+import gdown
 
 model = None
 
@@ -7,21 +8,29 @@ def load_model():
     global model
 
     if model is None:
-        model_path = os.path.join(
-            os.path.dirname(__file__),
-            "..",
-            "models",
-            "price_model.pkl"
-        )
-        model_path = os.path.abspath(model_path)
+        # ✅ Correct path
+        BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+        model_path = os.path.join(BASE_DIR, "models", "price_model.pkl")
 
-        model = joblib.load(model_path)
+        # ✅ Download if not exists
+        if not os.path.exists(model_path):
+            os.makedirs(os.path.dirname(model_path), exist_ok=True)
+
+            file_id = "1lsbY0WnZpz2YdxQ8e47YPp1aT4opRxtZ"
+            url = f"https://drive.google.com/uc?id={file_id}"
+
+            gdown.download(url, model_path, quiet=False)
+
+        # ✅ Load model (with error handling)
+        try:
+            model = joblib.load(model_path)
+        except Exception as e:
+            print("Error loading model:", e)
 
     return model
 
 
 def predict_price(data):
-
     model = load_model()
 
     features = [[
